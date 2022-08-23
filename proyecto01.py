1a745f26b9f100850d9a3cde6c9fd058fc2946dc
puigdaniel <64549156+puigdaniel@users.noreply.github.com>
2022-08-23 01:24:57 -0300

    Add files via upload

---
+++ proyecto01.py
@@ -0,0 +1,34 @@
+﻿#Hacer un juego que permita adivinar un numero al azar
+import random
+
+intentos = 0
+
+print("Hola: ¿como se llama usted?: ")
+nombre = input()
+
+numero = random.randint(1, 300)
+print("Bueno:  "+ nombre + " estoy pensando en un numero entre 1 y 300: ")
+i = 0
+while i < 6:
+	print("Atenti intenta adivinar: ")
+	estimacion = input()
+	estimacion = int(estimacion)
+	intentos = intentos + 1
+	
+	if estimacion < numero:
+		print("Tu estimacion es muy baja:")
+		
+	if estimacion > numero:
+		print("Tu estimacion es muy alta")
+		
+	if estimacion == numero:
+		break
+	
+	if estimacion == numero:
+		intentos = str(intentos)
+		print("Has ganado: " + nombre + "has adivinado en: " + intentos)
+		
+	if estimacion != numero:
+		numero = str(numero)
+		print("Has perdido brother, el numero que estabas pensando era: " + numero)
+	break
---
+++ proyecto02.py
@@ -0,0 +1,28 @@
+﻿print("¿Que sale de la cruza entre un mono y un pato?")
+input()
+
+print("Un monopatin")
+print()
+
+print("¿Por que vuelan los pajaros hacia el sur?")
+input()
+
+print("Porque caminando tardan un montón")
+print()
+
+print("¿En que se parecen una familia, un bombero y un barco?")
+input()
+
+print("Ni idea..., ¿en que se parecen? XD")
+input()
+
+print("En que el bombero y el barco tienen casco como las motos")
+
+print("¿Y la familia?  ", "end = '' ")
+
+print("Bien gracias\n")
+
+print("¿Que le dice la lechuga al tomate?")
+input()
+
+print("Nos vemos en la ensalada?")
